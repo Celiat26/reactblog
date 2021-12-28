5575cfd3ee0bcd21ae93f975e6fa6cb910e7841a
Lya To <tolya69100@gmail.com>
2021-12-28 16:28:25 +0100

    projet blog

---
+++ src/pages/Author.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+const Author = () => {
+    return (
+        <div>
+            <Navigation/>
+            <h1>Auteur : John Rambo</h1>
+        </div>
+    );
+};
+
+export default Author;
